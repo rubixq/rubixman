0b7d437c4d93a6ed2fb13fc4d234214e528c1aa1
Edward Pie <ep@hellofresh.com>
2018-08-20 18:52:03 +0200

    adds router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import AuthComponent from './Containers/Auth/AuthComponent';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <button className="btn btn-primary">Rubix Manager</button>
-      </div>
+        <BrowserRouter>
+          <Route path="/" exact component={AuthComponent}></Route>
+        </BrowserRouter>
     );
   }
 }
---
+++ src/Containers/Auth/AuthComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './AuthComponent.css';
+
+class AuthComponent extends Component{
+
+    render(){
+        return(
+            <div className="login-form-container">
+                <div className="login-form">
+                    LOGIN FORM HERE
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AuthComponent;
