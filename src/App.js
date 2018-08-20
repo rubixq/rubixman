dcee28f509aabd1e68f61a1c091ead8c35896675
Edward Pie <ep@hellofresh.com>
2018-08-20 18:08:07 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <button className="btn btn-primary">Rubix Manager</button>
+      </div>
+    );
+  }
+}
+
+export default App;
